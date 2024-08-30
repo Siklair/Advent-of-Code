89696fbfeb1e5ad6e0f425b4c1083adb872193ea
Siklair <hamoir.alex@gmail.com>
2024-08-31 01:04:23 +0200

    Update aoc template.

--- template.rs
+++ template.rs
@@ -3,6 +3,8 @@ use std::path::Path;
 use std::fs::File;
 use std::io::{self, BufRead};
 
+const INPUT_FILE_NAME: &str = "input.txt";
+
 fn main() {
     let file = env::current_dir().unwrap()
         .parent().unwrap()
@@ -14,9 +16,13 @@ fn main() {
         for line in lines.map_while(Result::ok) {
 
         }
+    } else {
+        println!("File not found: {}", INPUT_FILE_NAME);
     }
 
-    println!("no result");
+    let mut result: usize = 0;
+
+    println!("Result: {}", result);
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
